55c33f97742aeebf8fa93a163ca110f4b7e953af
Brody14 <stefania.spinas@gmail.com>
2023-05-02 15:33:09 +0200

    FEAT milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
 <script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  methods: {
+    fetchProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects')
+        .then(res => {
+          console.log(res.data.results)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  },
+  mounted() {
+    this.fetchProjects()
+  }
+}
 
 </script>
 
 <template>
   <div class="container">
-
+    <h1 class="title">Projects</h1>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+@use './style/general.scss';
+
+.title {
+  text-align: center;
+}
+
+.container {
+  padding: 20px;
+}
+</style>
+
